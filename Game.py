7e326122d0661fa63834eca55b777e9217427a26
BuildTools <unconfigured@null.spigotmc.org>
2022-11-13 08:43:43 -0600

    More work in progress, tried rotating

--- Game.py
+++ Game.py
@@ -8,11 +8,11 @@ win = pygame.display.set_mode((screen_width, screen_hight))
 pygame.display.set_caption("Shooter game")
 clock = pygame.time.Clock()
 run = True
-player1 = Player(win, screen_width, screen_hight, 1,name='Pawkel')
-player2 = Player(win, screen_width, screen_hight, 2,name='Mlk99')
+player1 = Player(win, screen_width, screen_hight, 1,name='Mlk99')
+player2 = Player(win, screen_width, screen_hight, 2,name='Pawkel')
 
 while run:
-    win.fill((0,255,255))
+    win.fill((80,100,110))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
@@ -21,7 +21,7 @@ while run:
     win.blit(player2.player, (player2.x, player2.y))
     keys = pygame.key.get_pressed()
     if keys[pygame.K_c]:
-        controls = font.render('P1 Arrow keys, P2 WASD', True, (0,0,0))
+        controls = font.render('P1 8456 Enter to shoot, P2 WASD Space to shoot', True, (0,0,0))
         win.blit(controls, (20,20))
     if keys[pygame.K_n]:
         p1name = font.render(str(player1.name), True, (0,0,0))
@@ -31,4 +31,4 @@ while run:
     player1.update(keys)
     player2.update(keys)
     pygame.display.update()
-    clock.tick(30)
+    clock.tick(40)
--- Game_Classes.py
+++ Game_Classes.py
@@ -18,6 +18,7 @@ class Player(object):
         self.speed = 5
         self.gold = 100
         self.id = id
+        self.health = 3
         self.name = f'P{id} '
         self.weapons = []
         self.activeWeaponIDs = []
@@ -28,7 +29,7 @@ class Player(object):
             self.name = f'Player{id}'
         else:
             self.name += name
-        skin = random.randrange(1,len(self.players_skins))
+        skin = random.randrange(0,len(self.players_skins))
         player = pygame.image.load(self.players_skins[skin])
         self.player = pygame.transform.scale(player, (200, int(200*scalingFactor)))
         self.controlOptions()
@@ -37,10 +38,12 @@ class Player(object):
     def controlOptions(self):
         if self.id == 1:
             self.controls= {'Up': pygame.K_w,'Left':pygame.K_a,\
-                'Right':pygame.K_d,'Down':pygame.K_s,'Shoot':pygame.K_e}
+                'Right':pygame.K_d,'Down':pygame.K_s,'Shoot':pygame.K_SPACE,'RotateRight':pygame.K_e\
+                    ,'RotateLeft':pygame.K_q}
         else:
-            self.controls = {'Up': pygame.K_UP,'Left':pygame.K_LEFT,\
-                'Right':pygame.K_RIGHT,'Down':pygame.K_DOWN,'Shoot':pygame.K_KP0}
+            self.controls = {'Up': pygame.K_KP8,'Left':pygame.K_KP4,\
+                'Right':pygame.K_KP6,'Down':pygame.K_KP5,'Shoot':pygame.K_KP_ENTER,'RotateRight':pygame.K_KP9\
+                    ,'RotateLeft':pygame.K_KP7}
 
     def update(self,keys):
         if keys[self.controls['Up']]:
@@ -53,6 +56,12 @@ class Player(object):
             self.move_w(1)
         elif keys[self.controls['Shoot']]:
             self.fight()
+        elif keys[self.controls['RotateRight']]:
+            for w in self.weapons:
+                pygame.transform.rotate(w.gun_skin, 20)
+        elif keys[self.controls['RotateLeft']]:
+            for w in self.weapons:
+                pygame.transform.rotate(w.gun_skin, -20)
         self.updateWeapons()
         self.updateWeaponStatus()
 
@@ -63,7 +72,7 @@ class Player(object):
                 self.activeWeaponIDs.remove(wid)
     def updateWeapons(self):
         for w in self.weapons:
-            w.x,w.y = self.x+40, self.y
+            w.x,w.y = self.x+45, self.y
             self.win.blit(w.gun_skin, (w.x, w.y))
     def move_w(self,sign=1):
         ''' Move right or left'''
@@ -83,7 +92,7 @@ class Player(object):
             
     def AddWeapon(self):
         pistol = Gun(self.win, 'Pistol', '10mm', 2,' ', 'Pistol.png', 'Pistol_bullet.png')
-        pistol.x,pistol.y = self.x+40, self.y
+        pistol.x,pistol.y = self.x+45, self.y
         self.weapons.append(pistol)
     
     def fight(self, weaponID=0):
@@ -113,24 +122,23 @@ class Gun(object):
         self.win = win
         sc_w, sc_h = 1920, 1080
         scalingFactor = sc_h / sc_w
-        gun_skin = pygame.image.load(skin)
-        self.gun_skin = pygame.transform.scale(gun_skin, (200, int(200*scalingFactor)))
-        bullet = pygame.image.load(bullet_look)
-        self.bullet = pygame.transform.scale(bullet, (200, int(200*scalingFactor)))
+        skin = pygame.image.load(skin)
+        self.gun_skin = pygame.transform.scale(skin, (200, int(200*scalingFactor)))
+        bullet_skin = pygame.image.load(bullet_look)
+        self.bullet = pygame.transform.scale(bullet_skin, (200, int(200*scalingFactor)))
         self.fire_speed = fire_speed
 
     def shoot(self):
         if self.ammo_amount >0:
             self.bullet_x,self.bullet_y = self.x, self.y
             self.draw_bullet()
-            self.ammo_amount-=1
+            # self.ammo_amount-=1
 
     def draw_bullet(self):
         self.win.blit(self.bullet,(self.bullet_x,self.bullet_y))
 
     def updateGameloop(self):
         self.bullet_x +=1
-        self.bullet_y +=1
         if self.bullet_x > self.win.get_width() or self.bullet_y > self.win.get_height() \
             or self.bullet_x <0  or self.bullet_y <0:
             return 1
