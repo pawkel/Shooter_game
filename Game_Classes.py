1f3148e185aeae7004716bea112e910bc37d8c23
ZeitgeberH <wukong.flamingo@gmail.com>
2022-11-20 16:06:15 -0600

    gun and bullet rotation

--- Game_Classes.py
+++ Game_Classes.py
@@ -1,10 +1,15 @@
 import pygame
 import random
 import numpy as np
+from PIL import Image,ImageOps
 # class Gane():
 #     def __init__(self) -> None:
 #         pass
 #     def update(self):
+def pilImageToSurface(pilImage):
+    return pygame.image.fromstring(
+        pilImage.tobytes(), pilImage.size, pilImage.mode).convert()
+
 
 class Player(object):
     def __init__(self,win, screenwidth, screenhight, id,name=''):
@@ -58,10 +63,10 @@ class Player(object):
             self.fight()
         elif keys[self.controls['RotateRight']]:
             for w in self.weapons:
-                pygame.transform.rotate(w.gun_skin, 20)
+                 w.rotate(5)
         elif keys[self.controls['RotateLeft']]:
             for w in self.weapons:
-                pygame.transform.rotate(w.gun_skin, -20)
+                 w.rotate(-5)
         self.updateWeapons()
         self.updateWeaponStatus()
 
@@ -70,10 +75,14 @@ class Player(object):
             weaponStatus = self.weapons[wid].updateGameloop()
             if weaponStatus:
                 self.activeWeaponIDs.remove(wid)
+
     def updateWeapons(self):
         for w in self.weapons:
-            w.x,w.y = self.x+45, self.y
+            # w.x,w.y = self.x+45, self.y
+            w.x = self.x + int(45*np.cos(w.angle/180*np.pi))
+            w.y= self.y + int(45*np.sin(w.angle/180*np.pi))
             self.win.blit(w.gun_skin, (w.x, w.y))
+
     def move_w(self,sign=1):
         ''' Move right or left'''
         self.x += self.speed*sign
@@ -119,26 +128,56 @@ class Gun(object):
         self.x = -10
         self.y = -10
         self.bullet_x,self.bullet_y = 0, 0
+        self.bullet_distance = 0
         self.win = win
         sc_w, sc_h = 1920, 1080
         scalingFactor = sc_h / sc_w
+        self.scalingFactor = scalingFactor
+        self.angle = 0
+        self.imageFileName = skin
         skin = pygame.image.load(skin)
         self.gun_skin = pygame.transform.scale(skin, (200, int(200*scalingFactor)))
         bullet_skin = pygame.image.load(bullet_look)
         self.bullet = pygame.transform.scale(bullet_skin, (200, int(200*scalingFactor)))
         self.fire_speed = fire_speed
 
+    def rotate(self, angle = 0):
+        im = Image.open(self.imageFileName)
+        self.angle += angle
+        if self.angle < 0 :
+            self.angle +=360
+        self.angle = np.mod(self.angle, 360)
+        x0,y0 = im.size[0]//2,im.size[1]//2
+        im_rotate = im.rotate(-self.angle, center=(x0,y0),
+         expand=False)
+        x1, y1 = im.size[0]//2,im.size[1]//2
+        im_rotate = im_rotate.crop((x1-x0, y1-y1, x1+x0, y1+y0))
+        # if abs(self.angle) < 270:
+        #     im_rotate = ImageOps.flip(im_rotate)
+        #     print('flip', self.angle)
+        im_fn = self.imageFileName.split('.png')[0]+"rotated.png"
+        im_rotate.save(im_fn)
+        skin = pygame.image.load(im_fn)
+        self.gun_skin = pygame.transform.scale(skin, (200,int(200*self.scalingFactor)))
+
+
     def shoot(self):
         if self.ammo_amount >0:
             self.bullet_x,self.bullet_y = self.x, self.y
+            self.bullet_distance = 0
             self.draw_bullet()
             # self.ammo_amount-=1
+
 
     def draw_bullet(self):
         self.win.blit(self.bullet,(self.bullet_x,self.bullet_y))
 
     def updateGameloop(self):
-        self.bullet_x +=1
+        self.bullet_distance +=1
+        dx = int(self.bullet_distance*np.cos(self.angle/180*np.pi))
+        dy= int(self.bullet_distance*np.sin(self.angle/180*np.pi))
+        self.bullet_x +=dx
+        self.bullet_y +=dy
         if self.bullet_x > self.win.get_width() or self.bullet_y > self.win.get_height() \
             or self.bullet_x <0  or self.bullet_y <0:
             return 1
